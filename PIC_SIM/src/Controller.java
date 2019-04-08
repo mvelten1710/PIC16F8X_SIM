fe961e6db18bdd06cb30e7a5fc91615cdd62cf85
mvelten1710 <marcovelten98@gmail.com>
2019-04-08 13:03:05 +0200

    Parser finished!
    Parser finished. Next step is to extract the specific things from the
    commands

--- PIC_SIM/src/Controller.java
+++ PIC_SIM/src/Controller.java
@@ -1,6 +1,21 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Controller {
 
-	
-	int commandMem[];
+	public static List<Integer> commandsList = new ArrayList<Integer>();
+	public static void main(String[] args) throws IOException {
+		//Starts the UI and combines the Decoder(Commands)
+		//and the Parser(Reads LST Files)
+		
+		//New Parser Object
+		Parser parser = new Parser();
+		//Adds all Values from Parser to the commandsList in Controller
+		commandsList.addAll(parser.getCommands());
+		
+		for (Integer integer : commandsList) {
+			System.out.println(integer);
+		}
+	}
 }
